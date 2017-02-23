6b256a4aac823a13b7c02d0542a2b1205ae7f055
ZhanghanKe <kezhanghan@outlook.com>
2017-02-24 00:11:41 +0800

    modify the folder structure for Script/UI/MainMenu and Sences/UI

--- Assets/Scripts/UI/MainMenu/MusicCtrl.cs
+++ Assets/Scripts/UI/MainMenu/MusicCtrl.cs
@@ -5,14 +5,16 @@ using UnityEngine.UI;
 
 public class MusicCtrl : MonoBehaviour {
 	private Scrollbar bar;
-	public volume Volume;
+	public volume volume;
 	// Use this for initialization
 	void Start () {
 		bar = this.gameObject.GetComponent<Scrollbar> ();
+
+        // fixme(Petrus): need instance Volume
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Volume.vol = bar.value;	
+		volume.vol = bar.value;	
 	}
 }
