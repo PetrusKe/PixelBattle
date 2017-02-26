9b46ced55c03fbd3d19ed40eba65c3f8a61b9f8e
FGStudio <1215075267@qq.com>
2017-02-26 13:40:31 +0800

    Aun2 (#8)

---
+++ Assets/Scripts/UI/MainMenu/switchScenedaynight.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class switchScenedaynight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+
+	void switchsc1()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("CityDark");
+	}
+
+}
