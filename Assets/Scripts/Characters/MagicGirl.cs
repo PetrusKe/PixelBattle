246638580008a25c294ff9a3fd1ebdb66ee92cda
Zhanghan Ke <kezhanghan@outlook.com>
2017-02-26 18:32:53 +0800

    add dash skills (#10)

--- Assets/Scripts/Characters/MagicGirl.cs
+++ Assets/Scripts/Characters/MagicGirl.cs
@@ -10,16 +10,16 @@ namespace GameCharacters
         public override void LightAttack()
         {
             base.LightAttack();
-            if (characterSkills.lightAttackAttrs.waitTime >= 0)
+            if (skills.lightAttackAttrs.waitTime >= 0)
                 return;
 
             state.IsLightAttack = true;   // may bug
             ChangeAnim(anim.lightAttack);
-            if (characterWeaponsCenter.Length == 0)
+            if (weaponCenter.Length == 0)
                 return;
 
-            LightAttackAttrs skillAttrs = characterSkills.lightAttackAttrs;
-            GameObject magicBall = Instantiate(extendObj.lightAttackObj, characterWeaponsCenter[0].position, characterWeaponsCenter[0].rotation) as GameObject;
+            LightAttackAttrs skillAttrs = skills.lightAttackAttrs;
+            GameObject magicBall = Instantiate(extendObj.lightAttackObj, weaponCenter[0].position, weaponCenter[0].rotation) as GameObject;
             MagicBallParicles paricles = magicBall.transform.GetComponent<MagicBallParicles>();
 
             paricles.owner = gameObject;
@@ -28,24 +28,26 @@ namespace GameCharacters
             paricles.force = skillAttrs.force;
             paricles.speed = skillAttrs.speed;
 
-            characterSkills.lightAttackAttrs.waitTime = 0;
+            skills.lightAttackAttrs.waitTime = 0;
         }
 
         protected override void GetWeaponsCenter()
         {
             base.GetWeaponsCenter();
             Transform leftWeaponCenter = transform.Find("Body/ArmL/HandL/_holder/Weapon/HitCenter");
-            characterWeaponsCenter = new Transform[1] { leftWeaponCenter };
+            weaponCenter = new Transform[1] { leftWeaponCenter };
 
         }
 
         public override void CheckCoolTime()
         {
-            if (characterSkills.lightAttackAttrs.waitTime < characterSkills.lightAttackAttrs.coolTime
-                && characterSkills.lightAttackAttrs.waitTime >= 0)
-                characterSkills.lightAttackAttrs.waitTime += Time.deltaTime;
-            else if (characterSkills.lightAttackAttrs.waitTime >= characterSkills.lightAttackAttrs.coolTime)
-                characterSkills.lightAttackAttrs.waitTime = -1;
+            base.CheckCoolTime();
+
+            if (skills.lightAttackAttrs.waitTime < skills.lightAttackAttrs.coolTime
+                && skills.lightAttackAttrs.waitTime >= 0)
+                skills.lightAttackAttrs.waitTime += Time.deltaTime;
+            else if (skills.lightAttackAttrs.waitTime >= skills.lightAttackAttrs.coolTime)
+                skills.lightAttackAttrs.waitTime = -1;
         }
 
     }
