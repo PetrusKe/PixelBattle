e14b6029cd390c7b75fe8a5fa07d6d6c8e4dd340
ZhanghanKe <kezhanghan@outlook.com>
2017-03-02 02:51:35 +0800

    add some skill particles

--- Assets/Scripts/Characters/MagicGirl/MagicGirl.cs
+++ Assets/Scripts/Characters/MagicGirl/MagicGirl.cs
@@ -7,20 +7,20 @@ namespace GameCharacters
 {
     public class MagicGirl : Character
     {
-        public override void LightAttack()
+        public override bool LightAttack()
         {
-            base.LightAttack();
-
+            if (!base.LightAttack())
+                return false;
             if (skills.lightAttackAttrs.waitTime >= 0)
-                return;
+                return false;
             if (weaponCenter.Length == 0)
-                return;
+                return false;
 
             state.IsLightAttack = true;
             ChangeAnim(anim.lightAttack);
 
             LightAttackAttrs skillAttrs = skills.lightAttackAttrs;
-            GameObject magicBall = Instantiate(extendObj.lightAttackObj, weaponCenter[0].position, weaponCenter[0].rotation) as GameObject;
+            GameObject magicBall = Instantiate(extendObj.lightAttackParticle, weaponCenter[0].position, weaponCenter[0].rotation) as GameObject;
             MagicBallParicles paricles = magicBall.transform.GetComponent<MagicBallParicles>();
 
             paricles.owner = gameObject;
@@ -30,23 +30,58 @@ namespace GameCharacters
             paricles.speed = skillAttrs.speed;
             paricles.radius = skillAttrs.radius;
             skills.lightAttackAttrs.waitTime = 0;
+            return true;
         }
 
-        public override void HardAttack()
+        public override bool HardAttack()
         {
-            base.HardAttack();
+            if (!base.HardAttack())
+                return false;
             if (skills.hardAttackAttrs.waitTime >= 0)
-                return;
+                return false;
             if (weaponCenter.Length == 0)
-                return;
+                return false;
 
             state.IsHardAttack = true;
             ChangeAnim(anim.hardAttack);
 
             HardAttackAttrs skillAttrs = skills.hardAttackAttrs;
-
             skills.hardAttackAttrs.waitTime = 0;
+            return true;
         }
+
+        public override bool Dash()
+        {
+            if (!base.Dash())
+                return false;
+
+            state.IsDash = true;
+
+            if (extendObj.dashParticle != null)
+            {
+                Vector3 pos = transform.position - new Vector3(0f, 0.5f, 0f);
+                GameObject particle = Instantiate(extendObj.dashParticle, pos, Quaternion.Euler(new Vector3(-90f, 0f, -90f))) as GameObject;
+                Destroy(particle, 1f);
+            }
+
+            Vector3 movement = -transform.TransformDirection(Vector3.forward).normalized;
+            movement = movement * skills.dashAttrs.distance;
+
+            Hashtable args = new Hashtable();
+            args.Add("speed", skills.dashAttrs.speed);
+            args.Add("easetype", skills.dashAttrs.moveWay);
+            args.Add("onstart", "ChangeDashAnim");
+            args.Add("onstartparams", true);
+            args.Add("oncomplete", "ChangeDashAnim");
+            args.Add("oncompleteparams", false);
+            args.Add("position", transform.position + movement);
+
+            iTween.MoveTo(gameObject, args);
+            skills.dashAttrs.waitTime = 0;
+            state.IsDash = false;
+            return true;
+        }
+
 
         protected override void GetWeaponsCenter()
         {
